a1965c1eec79294fc327ca0331df7b23171a259e
Rafael Schmid <rafaelfschmid@gmail.com>
2018-07-11 19:34:42 -0400

    time 512 tastas

--- scripts/costall.sh
+++ scripts/costall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 input=$1 #input files dir
 
+t=512
+
 echo "minmin"
 for m in 2 4 8 16 ; do
-	((t = 1024))
 	echo ${m}x${t}
 	for i in 0 1 2 3 4 5 6 7 8 9 ; do
 		java -cp "../jars/cloudsim-3.0.3.jar:../jars/cloudsim-3.0.3-sources.jar:." -Djava.library.path=. CloudSimCost minmin ${input}/machines2/${m}.in ${input}/tasks/${t}_${i}.in last 
@@ -13,7 +14,6 @@ done
 
 echo "maxmin"
 for m in 2 4 8 16 ; do
-	((t = 1024))
 	echo ${m}x${t}
 	for i in 0 1 2 3 4 5 6 7 8 9 ; do 
 		java -cp "../jars/cloudsim-3.0.3.jar:../jars/cloudsim-3.0.3-sources.jar:." -Djava.library.path=. CloudSimCost maxmin ${input}/machines2/${m}.in ${input}/tasks/${t}_${i}.in last
@@ -23,7 +23,6 @@ done
 
 echo "fcfs"
 for m in 2 4 8 16 ; do
-	((t = 1024))
 	echo ${m}x${t}
 	for i in 0 1 2 3 4 5 6 7 8 9 ; do
 		java -cp "../jars/cloudsim-3.0.3.jar:../jars/cloudsim-3.0.3-sources.jar:." -Djava.library.path=. CloudSimCost fcfs ${input}/machines2/${m}.in ${input}/tasks/${t}_${i}.in last
